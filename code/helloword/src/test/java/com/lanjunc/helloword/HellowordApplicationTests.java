7d5c6a7cfca3d29c5c3172b771e1ad5304dca89f
conglanjun <conglanjun@126.com>
2020-11-09 00:01:08 +0800

    [assign] yaml assign values

--- code/helloword/src/test/java/com/lanjunc/helloword/HellowordApplicationTests.java
+++ code/helloword/src/test/java/com/lanjunc/helloword/HellowordApplicationTests.java
@@ -1,13 +1,19 @@
 package com.lanjunc.helloword;
 
+import com.lanjunc.helloword.pojo.Person;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class HellowordApplicationTests {
 
+    @Autowired
+    private Person person;
+
     @Test
     void contextLoads() {
+        System.out.println(person);
     }
 
 }
